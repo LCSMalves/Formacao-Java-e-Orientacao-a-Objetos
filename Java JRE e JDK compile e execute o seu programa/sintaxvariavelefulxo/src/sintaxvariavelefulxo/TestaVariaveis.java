28a35775d67bc601a35e599932e14a768b2cb5b4
LCSMalves <lmalves10@gmail.com>
2022-12-06 06:55:50 -0300

    aula 4
    atividades aula 4

---
+++ Java JRE e JDK compile e execute o seu programa/sintaxvariavelefulxo/src/sintaxvariavelefulxo/TestaVariaveis.java
@@ -0,0 +1,24 @@
+package sintaxvariavelefulxo;
+
+public class TestaVariaveis {
+	public static void main(String[] args) {
+		
+		System.out.println("Ola mundo");
+		int idade;
+		idade = 37;
+		System.out.println(idade);
+		idade = 30 + 10;
+		System.out.println(idade);
+		idade = (7 * 5) + 2;
+		System.out.println(idade);		
+		System.out.println("a idade é " + idade);
+		
+		double salario;
+		salario = 1250.70;
+		System.out.println(salario);
+		
+		int valor = (int) salario;
+		System.out.println(valor);
+		
+	}
+}
